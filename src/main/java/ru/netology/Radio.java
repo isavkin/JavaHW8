1b850a4a0912cc0c3c10a26568e291dd9bcd8ddd
Ruslan <ruslan_savkin_nn@mail.ru>
2023-12-06 19:57:11 +0300

    Объекты с внутренним состоянием, управление состоянием при тестировании

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -3,37 +3,68 @@ package ru.netology;
 public class Radio {
     private int currentNumber;
     private int soundVolume;
-    public int getSoundVolume() {
-        return soundVolume;
+    private int maxCurrentNumber = 9;
+    private int minCurrentNumber = 0;
+    private int maxSoundVolume = 100;
+    private int minSoundVolume = 0;
+    private int sizeRadio;
+
+    public Radio(int sizeRadio) {
+        this.sizeRadio = sizeRadio - 1;
+        this.maxCurrentNumber = minCurrentNumber + sizeRadio;
+
     }
+
+    public Radio() {
+    }
+
+    //настройка станции
     public int getCurrentNumber() {
         return currentNumber;
     }
+    protected void setCurrentNumber(int newCurrentNumber) {
+        if (newCurrentNumber > maxCurrentNumber) {
+            return;
+        }
+        if (newCurrentNumber < minCurrentNumber) {
+            return;
+        }
+        currentNumber = newCurrentNumber;
+    }
+
+    public int getMaxCurrentNumber() {
+        return maxCurrentNumber;
+    }
+
+    public int getMinCurrentNumber() {
+        return minCurrentNumber;
+    }
 
     protected void next() {
-        if (currentNumber == 9) {
-            currentNumber = 0;
+        if (currentNumber >= maxCurrentNumber) {
+            currentNumber = minCurrentNumber;
         } else {
             currentNumber = currentNumber + 1;
         }
     }
 
     protected void prev() {
-        if (currentNumber == 0) {
-            currentNumber = 9;
+        if (currentNumber == minCurrentNumber) {
+            currentNumber = maxCurrentNumber;
         } else {
             currentNumber = currentNumber - 1;
         }
     }
 
-    protected void setCurrentNumber(int newCurrentNumber) {
-        if (newCurrentNumber > 9) {
-            return;
-        }
-        if (newCurrentNumber < 0) {
-            return;
-        }
-        currentNumber = newCurrentNumber;
+    //настройка громкости
+    public int getSoundVolume() {
+        return soundVolume;
+    }
+    public int getMinSoundVolume() {
+        return minSoundVolume;
+    }
+    public int getMaxSoundVolume() {
+        return maxSoundVolume;
     }
 
     protected void setSoundVolume(int newSoundVolume) {
@@ -41,8 +72,8 @@ public class Radio {
     }
 
     protected void volumeUp() {
-        if (soundVolume == 100) {
-            soundVolume = 100;
+        if (soundVolume >= maxSoundVolume) {
+            soundVolume = maxSoundVolume;
         } else {
 
             soundVolume = soundVolume + 1;
@@ -50,8 +81,8 @@ public class Radio {
     }
 
     protected void volumeDown() {
-        if (soundVolume == 0) {
-            soundVolume = 0;
+        if (soundVolume == minSoundVolume) {
+            soundVolume = minSoundVolume;
         } else {
             soundVolume = soundVolume - 1;
         }
